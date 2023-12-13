a7aaae8a762be9b0bdf1027b500beb2a50e60c1a
Mikhail Verbitskii <vemant@yandex.ru>
2023-12-13 16:45:43 +0300

    JS_5 all added

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,59 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+    if (!this.marks) {
+        console.log("Student has been expelled");
+        return;
+    }
+    this.marks.push(...marks);
 }
 
 Student.prototype.getAverage = function () {
-  
+    if(!this.marks || !this.marks.length) {
+        return 0;
+    }
+    const sum = this.marks.reduce((acc, curr) => acc + curr);
+    return sum / this.marks.length;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
 }
+
+
+//    this.setSubject = function(subjectName) {
+//        this.subject = subjectName;
+//    };
+//
+//    this.addMarks = function(...marksToAdd) {
+//        if (!this.marks) {
+//            console.log("Student has been expelled");
+//            return;
+//        }
+//        this.marks.push(...marksToAdd);
+//    };
+//
+//    this.getAverage = function() {
+//        if(!this.marks || !this.marks.length) {
+//            return 0;
+//        }
+//        const sum = this.marks.reduce((acc, curr) => acc + curr);
+//        return sum / this.marks.length;
+//    };
+//
+//    this.exclude = function(reason) {
+//        delete this.subject;
+//        delete this.marks;
+//        this.excluded = reason;
+//    };
